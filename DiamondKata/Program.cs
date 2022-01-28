c8b71b89cace365a9c686406910433c7f50ab939
David-Starer <97603488+David-Starer@users.noreply.github.com>
2022-01-28 15:24:25 +0000

    Add files via upload

---
+++ DiamondKata/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+// https://github.com/davidwhitney/CodeDojos/tree/master/Diamond%20Kata
+
+namespace DiamondKata
+{
+    public static class Program
+    {
+        private static void Main(string[] args)
+        {
+            var diamondGenerator = Startup.GetService<DiamondGenerator>();
+
+            char ? selectedCharacter;
+
+            do
+            {
+                Console.WriteLine("Enter A - Z");
+                selectedCharacter = diamondGenerator.GetSelectedCharacter();
+                if (!selectedCharacter.HasValue)
+                    Console.WriteLine($"{Environment.NewLine}Invalid input");
+                
+            } while (!selectedCharacter.HasValue);
+
+            try
+            {
+                var diamond = diamondGenerator.Generate(selectedCharacter.Value);
+
+                Console.Clear();
+                Console.WriteLine(string.Join(Environment.NewLine, diamond));
+                
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Console.Read();
+            }
+        }
+    }
+}
---
+++ DiamondKata/Startup.cs
@@ -0,0 +1,23 @@
+﻿using DiamondKata.Interfaces;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace DiamondKata
+{
+    public static class Startup
+    {
+        public static T GetService<T>()
+        {
+            return Host.CreateDefaultBuilder()
+                .ConfigureServices(ConfigureServices)
+                .ConfigureServices(services => services.AddSingleton<DiamondGenerator>())
+                .Build()
+                .Services
+                .GetService<T>();
+        }
+        private static void ConfigureServices(HostBuilderContext hostContext, IServiceCollection services)
+        {
+            services.AddSingleton<IConsoleReader, ConsoleReader>();
+        }
+    }
+}
